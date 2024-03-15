95e515a4b2d5566e1124bc1a75a204e445905f23
GarikBadalyan <garik.badalyan.1987@gmail.com>
2024-03-15 20:53:54 +0400

    First commit

---
+++ src/components/banner/bannerComponents.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="q-pa-md q-gutter-sm">
+    <q-banner inline-actions rounded class="bg-orange text-white">
+      You have lost connection to the internet. This app is offline.
+      <template v-slot:action>
+        <q-btn flat label="Turn ON Wifi" />
+        <q-btn flat label="Dismiss" />
+      </template>
+    </q-banner>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
